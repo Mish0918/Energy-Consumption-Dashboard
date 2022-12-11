938af08707af4829382b3f0db7d4bc9fc93862aa
Abhishek Mishra <mishraabhishek2@gmail.com>
2022-12-11 14:42:56 +0530

    Renaming the project

--- angular-energy-consumption-dashboard/src/app/app.component.spec.ts
+++ angular-energy-consumption-dashboard/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-attandance-tracker-dashboard'`, () => {
+  it(`should have as title 'energy-consumption-dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-attandance-tracker-dashboard');
+    expect(app.title).toEqual('energy-consumption-dashboard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-attandance-tracker-dashboard app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('energy-consumption-dashboard app is running!');
   });
 });
